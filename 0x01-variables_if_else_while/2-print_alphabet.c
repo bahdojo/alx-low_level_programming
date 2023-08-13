ac92ea83dc33a0852f16f917fedfe58da5907fad
bahdojo <ojofreshkeyzz@gmail.com>
2023-08-12 20:07:21 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always zero
+ */
+int main(void)
+{
+char i;
+for (i = 'a', i <= 'z' ; i++)
+putchar (i);
+putchar ('\n');
+return (0);
+
+}
