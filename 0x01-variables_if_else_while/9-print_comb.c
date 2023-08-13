1d34d9041de80f8134000e91d03906e5f415c868
bahdojo <ojofreshkeyzz@gmail.com>
2023-08-12 23:41:19 -0700

    commit for task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
+
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: always 0
+ *
+ * Return: always 0,
+ *
  */
 
 int main(void)
@@ -10,12 +14,11 @@ int main(void)
 	char letter;
 
 	for (num = 0; num < 10; num++)
-	putchar((num % 10) + '0');
+		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
-	putchar(letter);
+		putchar(letter);
 
 	putchar('\n');
 	return (0);
 }
-
